aca69cec78d6d5c13ae157191e897e3b6c6f6a20
vanshjangir <vanshjangir0001@gmail.com>
2023-05-29 08:10:17 +0530

    added tabs

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -26,8 +26,8 @@ local plugins = {
 
 	-- nui
 	"MunifTanjim/nui.nvim",
-    -- nerdtree
-    "preservim/nerdtree",
+	-- nerdtree
+	"preservim/nerdtree",
 
 	-- Statusline
 	{
@@ -188,6 +188,23 @@ local plugins = {
 			--   If not available, we use `mini` as the fallback
 			"rcarriga/nvim-notify",
 		},
+	},
+	{
+		"romgrk/barbar.nvim",
+		dependencies = {
+			"lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
+			"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+		},
+		init = function()
+			vim.g.barbar_auto_setup = false
+		end,
+		opts = {
+			-- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+			-- animation = true,
+			-- insert_at_start = true,
+			-- …etc.
+		},
+		version = "^1.0.0", -- optional: only update when a new 1.x version is released
 	},
 }
 
