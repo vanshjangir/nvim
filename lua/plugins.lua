c6f55264d604faa12cd305b65a9bde61f08e9832
vansh <vanshjangir0001@gmail.com>
2023-06-23 15:16:58 +0530

    removed noice cuz annoying

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -177,22 +177,7 @@ local plugins = {
 		end,
 	},
 
-	-- lazy.nvim
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		opts = {
-			-- add any options here
-		},
-		dependencies = {
-			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-			"MunifTanjim/nui.nvim",
-			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
-			--   If not available, we use `mini` as the fallback
-			"rcarriga/nvim-notify",
-		},
-	},
+	-- tabs
 	{
 		"romgrk/barbar.nvim",
 		dependencies = {
